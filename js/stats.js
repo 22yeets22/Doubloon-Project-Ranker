b8965c3d295e07a0bbef67942e2abaae17960565
Lekang Ji <132288501+22yeets22@users.noreply.github.com>
2024-12-14 11:13:34 -0800

    V4

--- js/stats.js
+++ js/stats.js
@@ -1,6 +1,6 @@
 import { doubloonShop, doubloonImg } from "./constants.js";
 
-let total, totalHours, rate, mean, median, stdev;
+let totalDoubloons, totalHours, rate, rates, mean, median, stdev;
 
 function stats() {
   const inputContainer = document.getElementById("input-container");
@@ -15,30 +15,31 @@ function stats() {
 
   if (doubloonsArray.length === 0 || hoursArray.length === 0) return;
 
-  total = doubloonsArray.reduce((sum, value) => sum + value, 0);
+  totalDoubloons = doubloonsArray.reduce((sum, value) => sum + value, 0);
   totalHours = hoursArray.reduce((sum, value) => sum + value, 0);
-  mean = total / doubloonsArray.length;
-  rate = total / totalHours;
+  rates = doubloonsArray.map(function (n, i) {
+    return n / hoursArray[i];
+  });
+  mean = totalDoubloons / doubloonsArray.length;
+  rate = totalDoubloons / totalHours;
 
   const sorted = [...doubloonsArray].sort((a, b) => a - b);
   const mid = Math.floor(sorted.length / 2);
   median = sorted.length % 2 === 0 ? (sorted[mid - 1] + sorted[mid]) / 2 : sorted[mid];
 
-  const variance =
-    doubloonsArray.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0) /
-    doubloonsArray.length;
+  const variance = rates.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0) / rates.length;
   stdev = Math.sqrt(variance);
 
   document.getElementById("average-rate").innerHTML = `Average Rate: ${rate.toFixed(
+    2
+  )} ${doubloonImg}/hour`;
+  document.getElementById("stdev-doubloon").innerHTML = `Standard Deviation: ${stdev.toFixed(
     2
   )} ${doubloonImg}/hour`;
   document.getElementById("average-doubloon").innerHTML = `Average: ${mean.toFixed(
     2
   )} ${doubloonImg}/project`;
   document.getElementById("median-doubloon").innerHTML = `Median: ${median.toFixed(
-    2
-  )} ${doubloonImg}/project`;
-  document.getElementById("stdev-doubloon").innerHTML = `Standard Deviation: ${stdev.toFixed(
     2
   )} ${doubloonImg}/project`;
   document.getElementById("stats").style = "visibility: visible;";
@@ -59,9 +60,9 @@ function timeCalculate() {
   let doubloonsNeeded;
   if (doubloonDropdown.value === "---") {
     // Use the number input instead
-    doubloonsNeeded = parseFloat(doubloonsInput.value) - total;
+    doubloonsNeeded = parseFloat(doubloonsInput.value) - totalDoubloons;
   } else if (doubloonDropdown.value in doubloonShop) {
-    doubloonsNeeded = doubloonShop[doubloonDropdown.value] - total;
+    doubloonsNeeded = doubloonShop[doubloonDropdown.value] - totalDoubloons;
   } else {
     timeCalcResult.innerHTML = `Invalid ${doubloonImg}`;
     return;
